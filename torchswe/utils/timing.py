e9f92baecd80171c7002d92ff856a0a24bb78102
Rohan Yadav <rohany@alumni.cmu.edu>
2023-09-07 09:45:15 -0700

    torchswe/utils: flush scheduling window before timing

--- torchswe/utils/timing.py
+++ torchswe/utils/timing.py
@@ -1,7 +1,14 @@
 import os
 
 if "LEGATE_MAX_DIM" in os.environ and "LEGATE_MAX_FIELDS" in os.environ:
-    from legate.timing import time
+    from legate.timing import time as _time
+    from legate.core.runtime import runtime
+
+    def time():
+        # Ensure that all pending operations are flushed
+        # before we start timing.
+        runtime.flush_scheduling_window()
+        return _time()
 else:
     import time as t
 
