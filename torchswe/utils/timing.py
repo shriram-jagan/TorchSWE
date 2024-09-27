e89b4259cc0d2216bb110eeeb980913160fbd6fe
Shriram <83928561+shriram-jagan@users.noreply.github.com>
2024-09-27 13:56:05 -0700

    Update pydantic version requirement and remove flushing the window since legate doesn't expose that API anymore.

--- torchswe/utils/timing.py
+++ torchswe/utils/timing.py
@@ -2,12 +2,8 @@ import os
 
 if "LEGATE_MAX_DIM" in os.environ and "LEGATE_MAX_FIELDS" in os.environ:
     from legate.timing import time as _time
-    from legate.core.runtime import runtime
 
     def time():
-        # Ensure that all pending operations are flushed
-        # before we start timing.
-        runtime.flush_scheduling_window()
         return _time()
 else:
     import time as t
